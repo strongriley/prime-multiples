7cddf634f6096ae92be4c7224acf5fb82ef1dbce
Riley Strong <eponymous@rileystrong.com>
2017-03-16 00:10:47 +0800

    First pass seems to be working

--- prime_multiples.rb
+++ prime_multiples.rb
@@ -1,12 +1,79 @@
 #!/usr/bin/env ruby
 DEFAULT_PRIME_COUNT = 10
+FIRST_PRIME = 2 # Have to seed
 
-def find_primes; end
+# TODO(riley): Not the cleanest but don't really want to implement the
+# segmented sieve and assume it'd be stupid to print a 90x90 prime multiple
+# grid
+# https://primes.utm.edu/lists/small/1000.txt
+MAX_SIEVE_INTEGER = 500
+MAX_PRIME_COUNT = 90
+MAX_I = Math.sqrt(MAX_SIEVE_INTEGER)
 
-def multiply_primes(primes); end
+class PrimeMultiples
+  class << self
+    def find_primes(count)
+      # Implements Sieve of Eratosthenes
+      # https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+      # Complexity O(n log log n) but will be faster with lower prime count
+      found_primes = []
+      integers = [true] * (MAX_SIEVE_INTEGER + 1) # zero
+      (FIRST_PRIME..MAX_SIEVE_INTEGER).each  do |i|
+        next unless integers[i]
+        found_primes << i
+        break if found_primes.length == count
+        next if i > MAX_I
+        mark_multiples(i, integers)
+      end
+      found_primes
+    end
 
-def print_prime_multiples(count = DEFAULT_PRIME_COUNT)
-  puts "TODO #{count}"
+    def multiply_primes(primes)
+      # Create a square grid for all the prime multiples and labels
+      # TODO(riley): think time complexity is n^1.5 with optimization
+      # If we visited each combo it'd be n^2
+      multiples = (0..primes.size).collect { [nil] * (primes.size + 1) }
+      primes.each_with_index do |first_prime, i|
+        multiples[0][i + 1] = first_prime # column labels
+        multiples[i + 1][0] = first_prime # row labels
+        (i..primes.size - 1).each do |j|
+          together = first_prime * primes[j]
+          multiples[i + 1][j + 1] = multiples[j + 1][i + 1] = together
+        end
+      end
+      multiples
+    end
+
+    def main(count = DEFAULT_PRIME_COUNT)
+      primes = find_primes(count)
+      multiples = multiply_primes(primes)
+      print_multiples(multiples)
+    end
+
+    def print_multiples(multiples)
+      # Get max digits for spacing in grid
+      digit_size = (multiples[multiples.size - 1][multiples.size - 1]).to_s.size
+      multiples.each do |row|
+        row_text = ''
+        row.each do |num|
+          num_size = num.to_s.size
+          extra_spaces = ' ' * (digit_size - num_size)
+          row_text <<  "| #{num}#{extra_spaces} "
+        end
+        row_text << '|'
+        puts row_text
+        puts '-' * row_text.size
+      end
+    end
+
+    private
+
+    def mark_multiples(i, integers)
+      Range.new(i, MAX_SIEVE_INTEGER + 1).step(i).each do |excluded|
+        integers[excluded] = false
+      end
+    end
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -15,5 +82,5 @@ if __FILE__ == $PROGRAM_NAME
   rescue ArgumentError, TypeError
     count = DEFAULT_PRIME_COUNT
   end
-  print_prime_multiples(count)
+  PrimeMultiples.main(count)
 end
---
+++ spec/prime_multiples_spec.rb
@@ -0,0 +1,43 @@
+require_relative '../prime_multiples'
+
+describe PrimeMultiples do
+  describe '.find_primes' do
+    it 'returns just 2 given count=1' do
+      expect(PrimeMultiples.find_primes(1)).to eql([2])
+    end
+
+    it 'returns first ten primes given count=10' do
+      first_ten = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+      expect(PrimeMultiples.find_primes(10)).to eql(first_ten)
+    end
+
+    it 'returns first 20 primes given count=20' do
+      first_twenty = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+                      53, 59, 61, 67, 71]
+      expect(PrimeMultiples.find_primes(20)).to eql(first_twenty)
+    end
+  end
+
+  describe '.multiply_primes' do
+    it 'returns 2x2 grid given just 2' do
+      grid = [
+        [nil, 2],
+        [2,   4]
+      ]
+      expect(PrimeMultiples.multiply_primes([2])).to eql(grid)
+    end
+
+    it 'returns 3x3 grid given just primes 2, 3' do
+      grid = [
+        [nil, 2, 3],
+        [2,   4, 6],
+        [3,   6, 9]
+      ]
+      expect(PrimeMultiples.multiply_primes([2, 3])).to eql(grid)
+    end
+  end
+
+  describe '.print_multiples' do
+    # TODO(riley): how to test stdout?
+  end
+end
